5919d688f9b939e9b5bd83788d37bdd15eb9cb51
Wiam <wiamnasr@gmail.com>
2021-12-07 08:01:01 +0000

    added a button to toggle the order of scores

--- src/components/CountryScoreTable/CountryScoreTable.js
+++ src/components/CountryScoreTable/CountryScoreTable.js
@@ -1,13 +1,27 @@
 // Styles
 import { Wrapper } from "./CountryScoreTable.styles";
 
+// hooks
+import { useState } from "react";
+
 // components
 import allCountryScores from "../../../src/scores";
 import PlayerScore from "../PlayerScore/PlayerScore";
 
 const CountryScoreTable = () => {
+  // false for the initial descending state of the scores, true for ascending
+  const [scoresOrder, setScoresOrder] = useState(false);
+  console.log(scoresOrder);
+
+  // making sure my scoresOrder is updated with every button click to the opposite of what it was
+  const scoresOrderHandler = () => {
+    setScoresOrder(() => !scoresOrder);
+  };
+
   return (
     <Wrapper>
+      {/* adding a button on the top of the page that toggles the order of every high scores table between asc and desc */}
+      <button onClick={scoresOrderHandler}>Toggle Scores Order</button>
       {allCountryScores
         .sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0))
         .map((country) => {
@@ -17,7 +31,11 @@ const CountryScoreTable = () => {
               <table>
                 <tbody>
                   {country.scores
-                    .sort((a, b) => parseFloat(b.s) - parseFloat(a.s))
+                    .sort((a, b) =>
+                      scoresOrder
+                        ? parseFloat(a.s) - parseFloat(b.s)
+                        : parseFloat(b.s) - parseFloat(a.s)
+                    )
                     .map((score) => {
                       return (
                         <PlayerScore
